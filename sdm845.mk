afba8ad53fb4521d0bb33df00b99bd050401fb65
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    sdm845-common: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- sdm845.mk
+++ sdm845.mk
@@ -414,7 +414,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
-    libwpa_client \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
