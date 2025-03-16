fb87a686ea46215764ac10f7bd55163522709d01
Han Sol Jin <hansol@hansol.ca>
2025-03-15 19:34:59 -0700

    sdm845-common: don't build NXP NFC stack for style3lm
    It uses CXD22xx NFC, prebuilt
    
    Change-Id: Ic485328391214935897e055e3d7f723cf36752ef

--- sdm845.mk
+++ sdm845.mk
@@ -260,13 +260,18 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
-    Tag \
+    Tag
+
+ifeq ($(findstring style3lm,$(DEVICE_NAME)),)
+# NFC - NXP
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service \
     vendor.nxp.nxpese@1.0.vendor:64 \
     vendor.nxp.nxpnfc@1.0.vendor:64
+endif
 
 ifneq ($(filter judypn judyln judyp,$(DEVICE_NAME)),)
 PRODUCT_COPY_FILES += \
